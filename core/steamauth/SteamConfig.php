0b3ec394526a2456ffb79a3932490f6dec441ac9
James Finch <LubricantJam@users.noreply.github.com>
2022-09-05 00:47:23 +0100

    Update SteamConfig.php

--- core/steamauth/SteamConfig.php
+++ core/steamauth/SteamConfig.php
@@ -7,9 +7,9 @@ $password = "";
 
 //Version 4.0
 $steamauth['apikey'] = ""; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
-$steamauth['domainname'] = "http://localhost/nexusrp/"; // The main URL of your website displayed in the login page
-$steamauth['logoutpage'] = "http://localhost/nexusrp/"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
-$steamauth['loginpage'] = "http://localhost/nexusrp/admin/"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+$steamauth['domainname'] = ""; // The main URL of your website displayed in the login page
+$steamauth['logoutpage'] = ""; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+$steamauth['loginpage'] = ""; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 
 // System stuff
 if (empty($steamauth['apikey'])) {
